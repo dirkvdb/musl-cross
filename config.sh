f5e14f40ec67082605323be6badcb9560d8729a8
Dirk Vanden Boer <dirk.vdb@gmail.com>
2015-08-18 15:53:11 +0200

    Revert "Updated settings"
    This reverts commit baf09111b8ac23a57504dbfffb35400c87ea6864.

--- config.sh
+++ config.sh
@@ -11,16 +11,16 @@ CC_BASE_PREFIX=/opt/cross
 
 # If you use arm, you may need more fine-tuning:
 # arm hardfloat v7
-TRIPLE=arm-linux-musleabihf
-GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=neon-vfpv4"
-GCC_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=neon-vfpv4"
+#TRIPLE=arm-linux-musleabihf
+#GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
+#GCC_CONFFLAGS="--with-arch=armv7-a --with-float=hard --with-fpu=vfpv3-d16"
 
 # arm softfp
 #TRIPLE=arm-linux-musleabi
 #GCC_BOOTSTRAP_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
 #GCC_CONFFLAGS="--with-arch=armv7-a --with-float=softfp"
 
-MAKEFLAGS=-j4
+MAKEFLAGS=-j8
 
 # Enable this to build the bootstrap gcc (thrown away) without optimization, to reduce build time
 GCC_STAGE1_NOOPT=1
