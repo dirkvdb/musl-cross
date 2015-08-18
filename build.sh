008be283374faad710e020f589c55624235eab86
Dirk Vanden Boer <dirk.vdb@gmail.com>
2015-08-18 15:54:41 +0200

    Enable gold linker

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Build a cross-compiler
-# 
+#
 # Copyright (C) 2012 Gregor Richards
-# 
+#
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
 # copyright notice and this permission notice appear in all copies.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
 # REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
 # AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
@@ -23,7 +23,7 @@ fi
 # Fail on any command failing, show commands:
 set -ex
 
-BINUTILS_CONFFLAGS=
+BINUTILS_CONFFLAGS="--enable-gold"
 GCC_BOOTSTRAP_CONFFLAGS=
 MUSL_CONFFLAGS=
 GCC_CONFFLAGS=
